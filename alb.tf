f52a110d46228b7c42608034894822c687e892cc
Ciprian <cursu@axetrading.com>
2023-08-29 11:22:11 +0300

    adding access logs bucket

--- alb.tf
+++ alb.tf
@@ -3,21 +3,23 @@ module "alb" {
   source   = "axetrading/load-balancer/aws"
   version  = "1.3.2"
 
-  name                   = each.value.load_balancer_name
-  load_balancer_type     = each.value.load_balancer_type
-  http_listener_enabled  = try(each.value.http_listener_enabled, true)
-  https_listener_enabled = try(each.value.https_listener_enabled, true)
-  certificate_arn        = each.value.certificate_arn
-  create_security_group  = try(each.value.create_lb_security_group, true)
-  security_group_name    = try(each.value.create_lb_security_group, true) ? each.value.lb_security_group_name : null
-  security_group_rules   = try(each.value.create_lb_security_group, true) ? each.value.lb_security_group_rules : null
-  security_groups        = try(each.value.lb_additional_security_groups, [])
-  subnets                = each.value.lb_subnet_ids
-  target_groups          = each.value.target_groups_spec
-  vpc_id                 = each.value.vpc_id
-  enable_access_logs     = each.value.enable_access_logs
-  access_logs_bucket     = each.value.access_logs_bucket
-  access_logs_prefix     = each.value.access_logs_prefix
+  name                        = each.value.load_balancer_name
+  access_logs_bucket_name     = try(each.value.access_logs_bucket, null)
+  access_logs_prefix          = try(each.value.access_logs_prefix, null)
+  certificate_arn             = each.value.certificate_arn
+  create_access_logs_bucket   = try(each.value.create_access_logs_bucket, false)
+  create_security_group       = try(each.value.create_lb_security_group, true)
+  enable_access_logs          = try(each.value.enable_access_logs, false)
+  existing_access_logs_bucket = try(each.value.existing_access_logs_bucket, null)
+  http_listener_enabled       = try(each.value.http_listener_enabled, true)
+  https_listener_enabled      = try(each.value.https_listener_enabled, true)
+  load_balancer_type          = each.value.load_balancer_type
+  security_group_name         = try(each.value.create_lb_security_group, true) ? each.value.lb_security_group_name : null
+  security_group_rules        = try(each.value.create_lb_security_group, true) ? each.value.lb_security_group_rules : null
+  security_groups             = try(each.value.lb_additional_security_groups, [])
+  subnets                     = each.value.lb_subnet_ids
+  target_groups               = each.value.target_groups_spec
+  vpc_id                      = each.value.vpc_id
 
   tags = var.tags
 }
